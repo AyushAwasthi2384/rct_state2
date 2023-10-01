9e566ea30cd40bfb29fccc10e828dbee101a741c
AyushAwasthi2384 <ayushawasthicoding@gmail.com>
2023-10-01 18:38:39 +0530

    temp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            value:0
+        }
+    }
+    render() { 
+        return (
+            <div>
+                <button onClick={()=>this.setState({value : this.state.value +1})}>Click me</button>
+                <br/>
+                <label>{this.state.value}</label>
+            </div>
+        );
+    }
 }
 
-export default App;
+const root = document.querySelector("#root");
+ReactDOM.render(<App/>, root)
+
+export default App
